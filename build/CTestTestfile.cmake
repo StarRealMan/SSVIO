34d13d5490fc9fa093f9cb31a6925274f4d33304
StarRealMan <965669036@qq.com>
2020-11-20 19:18:21 +0800

    Rewrite git ignore

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/starydy/SemSeg_Slam/SSVIO
-# Build directory: /home/starydy/SemSeg_Slam/SSVIO/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("driver")
-subdirs("src")
-subdirs("app")
--- build/driver/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/starydy/SemSeg_Slam/SSVIO/driver
-# Build directory: /home/starydy/SemSeg_Slam/SSVIO/build/driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
